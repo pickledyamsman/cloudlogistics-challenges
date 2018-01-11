084c2a79a8eebdf08402e3fc05e7b3696edb9658
pickledyamsman <pickledyamsman@gmail.com>
2018-01-10 19:22:18 -0500

    Moves code to service for challenges 2 and 3

---
+++ addresses-challenge-3/app/services/cloud_logistics.rb
@@ -0,0 +1,75 @@
+# This file is used to start the CLI from the command line.
+class CloudLogistics
+  def call
+    run_cli
+    show_bill
+  end
+
+  private
+
+  def run_cli
+    @route = Route.new
+    stop_number = ask_stop_number
+    puts 'Carrier name:'
+    @route.carrier = gets.chomp
+    ask_stops(stop_number - 2, @route)
+    @route.save
+  end
+
+  #######################
+  # Input methods
+  #######################
+
+  def ask_stop_number
+    puts 'Number of stops:'
+    puts 'Stops must be 2 or higher.' while (stop_number = gets.chomp.to_i) < 2
+    stop_number
+  end
+
+  def ask_stops(stop_number, route)
+    puts 'Origin:'
+    route.stops << split_address(gets.chomp, 'Origin')
+    stop_number.times do |stop|
+      puts "#{(stop + 1).ordinalize} stop:"
+      route.stops << split_address(gets.chomp, "#{(stop + 1).ordinalize} stop")
+    end
+    puts 'Destination:'
+    puts 'Format (street address, city, state, zipcode, country)'
+    route.stops << split_address(gets.chomp, 'Destination')
+  end
+
+  def split_address(full_address, stop_type)
+    address, city, the_rest = full_address.split(', ', 3)
+    state, zipcode, country = the_rest.split(' ', 3)
+
+    Stop.create(address: address,
+                city: city,
+                state: state,
+                zipcode: zipcode,
+                country: country,
+                stop_type: stop_type)
+  end
+
+  #######################
+  # Output methods
+  #######################
+
+  def show_bill
+    puts "Bill of lading number #{@route.id}, "\
+         "being carried by #{@route.carrier}"
+
+    show_stops
+  end
+
+  def show_stops
+    @route.stops.each do |stop|
+      stop_formatter(stop)
+    end
+  end
+
+  def stop_formatter(stop)
+    puts " - #{stop.stop_type}: #{stop.address}, "\
+     "#{stop.city}, #{stop.state} #{stop.zipcode}, "\
+     "#{stop.country} #{stop.latitude}, #{stop.longitude}"
+  end
+end
---
+++ transportation-challenge-2/app/services/cloud_logistics.rb
@@ -0,0 +1,50 @@
+# This file is used to start the CLI from the command line.
+class CloudLogistics
+  def call
+    run_cli
+    show_output
+  end
+
+  private
+
+  def run_cli
+    @route = Route.new
+    @stops = ask_stop_number
+    @route.carrier = ask_question('Carrier name:')
+    @route.origin = ask_question('Origin:')
+    ask_stops(@stops - 2, @route)
+    @route.destination = ask_question('Destination:')
+    @route.save
+  end
+
+  def ask_stop_number
+    puts 'Number of stops:'
+    puts 'Stops must be 2 or higher.' while (stop_number = gets.chomp.to_i) < 2
+    stop_number
+  end
+
+  def ask_question(question)
+    puts question
+    gets.chomp
+  end
+
+  def ask_stops(stops, route)
+    stops.times do |stop|
+      puts "#{(stop + 1).ordinalize} stop:"
+      route.stops.new(address: gets.chomp)
+    end
+  end
+
+  def show_output
+    puts "Bill of lading number #{@route.id}, being carried by #{@route.carrier}"
+    puts " - Origin: #{@route.origin}"
+    show_stops
+    puts " - Destination: #{@route.destination}"
+  end
+
+  def show_stops
+    @route.stops.each_with_index do |stop, index|
+      puts " - #{(index + 1).ordinalize} stop: #{stop.address}"
+    end
+  end
+end
